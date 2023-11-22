4a79eca5cf8c0b3a74501b45b74e91857ca7e291
Davi J <davijosantos.dj@gmail.com>
2023-11-22 16:15:10 -0300

    First Commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <h1>
+      Hello World
+      <Head>
+        <title>Home | dashgo</title>
+      </Head>
+    </h1>
+  )
+}
